b151fbbc149ad826235f78cfaf80653d61095be8
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-05-17 21:18:25 -0500

    Added GET array of users

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = express();
 server.use(express.json());
 
 const totalUsers = [];
-
+//POST	/api/users	Creates a user using the information sent inside the request body.
 server.post("/api/users", (req,res) => {
     const users = req.body;
 
@@ -14,6 +14,13 @@ server.post("/api/users", (req,res) => {
     res.status(201).json(users);
 
 });
+//GET	/api/users	Returns an array users.
+server.get("/api/users/", (req,res) => {
+    res.status(200).json( { api:  {
+        "User 1": "Aaron",
+        "user 2": "John Cena",
+    }})
+})
 const PORT = 4000;
 
 
