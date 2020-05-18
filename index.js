1faf5292e182aaaf80c966ec4fb4077d79b0cf54
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-05-17 20:52:34 -0500

    created a server that stores the users data logged into the /api/users

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const server = express();
 
 server.use(express.json());
 
+const totalUsers = [];
+
+server.post("/api/users", (req,res) => {
+    const users = req.body;
+
+    users.id = shortid.generate();
+    totalUsers.push(users);
+    res.status(201).json(users);
+
+})
 const PORT = 5000;
 
 
